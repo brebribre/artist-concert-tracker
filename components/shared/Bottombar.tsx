69b70dd849d40eb1755f609c67d3a0840fd54c28
brebribre <70563428+brebribre@users.noreply.github.com>
2023-09-16 16:22:27 +0200

    redeploy

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -5,6 +5,7 @@ function Bottombar(props:any){
     return (
         <div className = "bg-slate-800 fixed bottom-0 w-full py-2">
             <p className = "text-sm text-slate-500 flex justify-center">©Bryan Alvin 2023</p>
+            
         </div>
     )
 }
