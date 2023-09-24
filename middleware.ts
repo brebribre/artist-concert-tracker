c44ae2dbbcaec6230753adb080e8bd0d6eebf808
brebribre <70563428+brebribre@users.noreply.github.com>
2023-09-24 17:17:48 +0200

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: ['/'],
+    publicRoutes: ['/','/artists', '/girlgroup/','/boygroup/'],
     ignoredRoutes: ['/api/webhook/clerk']
 });
 
